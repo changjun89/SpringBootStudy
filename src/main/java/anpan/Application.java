6549469deaf0cb3efc6620626193cef21ac83a99
DESKTOP-49C2M5J\LEE <leechang0423@naver.com>
2019-02-05 21:40:53 +0900

    자동설정의이해 학습

--- src/main/java/anpan/Application.java
+++ src/main/java/anpan/Application.java
@@ -1,11 +1,19 @@
 package anpan;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@Configuration
+//@ComponentScan
+//@EnableAutoConfiguration
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class,args);
+        //SpringApplication.run(Application.class,args);
+        SpringApplication app = new SpringApplication(Application.class);
+        app.setWebApplicationType(WebApplicationType.NONE);
+        app.run(args);
     }
 }
